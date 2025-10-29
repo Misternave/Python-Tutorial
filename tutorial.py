2bf8ef3e4bf955492ae6e5ebd0650308bc967819
Misternave <tiagonaveribeiro@gmail.com>
2025-10-29 13:51:31 +0000

    first commit

---
+++ tutorial.py
@@ -0,0 +1,43 @@
+# line01 = "****************"
+# line02 = "*              *"
+# line03 = "*   WELCOME!   *"
+# line04 = "ddddddddd"
+# result = (1, 2, 3)
+
+
+# # fedd
+# print("")
+# print(line01)
+# print(line02)
+# print(line03)
+# print(line03)
+# print(line02)
+# print(line01)
+
+# Operators #
+# a = 1
+# b = 2
+# c = 3
+# d = 1
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a < b)
+# print(a > b)
+# print(a == a)
+# print(a != a)
+# print(a >= a)
+# print(a and b)
+# print(a and a)
+
+# IF Statement#
+meaning = 211
+print("")
+
+if meaning > 10:
+    print("Right on")
+else:
+    print("Not right")
+# Ternary Operator
+print("yes") if meaning > 10 else print("no")
